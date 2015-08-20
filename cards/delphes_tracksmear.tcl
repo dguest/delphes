aba28a1178fd5b06d881f6e89df69333e2f67aaf
Daniel Guest <dguest@cern.ch>
2015-08-19 23:43:42 -0700

    change parton labeling cone back to default

--- cards/delphes_tracksmear.tcl
+++ cards/delphes_tracksmear.tcl
@@ -519,7 +519,7 @@ module JetFlavorAssociation JetFlavorAssociation {
   set ParticleLHEFInputArray Delphes/allParticlesLHEF
   set JetInputArray JetEnergyScale/jets
 
-  set DeltaR 0.3
+  set DeltaR 0.5
   set PartonPTMin 1.0
   set PartonEtaMax 2.5
 
