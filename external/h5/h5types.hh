c2c6598d66e31fcb75034a77ea8b59cc83117a38
Daniel Guest <dguest@cern.ch>
2015-09-01 19:14:32 -0700

    more template magic

--- external/h5/h5types.hh
+++ external/h5/h5types.hh
@@ -1,31 +1,29 @@
 #ifndef H5_TYPES_HH
 #define H5_TYPES_HH
 
+#include "h5container.hh"
+
 #include "H5Cpp.h"
 #include <vector>
 #include <string>
 
 #define H5_INSERT(INTO, CLASS, MEMBER)					\
-  do {									\
-    using h5::insert;							\
-    insert(INTO, #MEMBER, offsetof(CLASS, MEMBER), &CLASS::MEMBER);	\
-  } while (false)
+  h5::insert(INTO, #MEMBER, offsetof(CLASS, MEMBER), &CLASS::MEMBER)
+
 
 namespace h5 {
   H5::DataType type(int);
   H5::DataType type(double);
   template <typename T>
-  H5::DataType type(const h5::vector<T>& dummy) {
-    T subdummy;
-    const auto subtype = type(subdummy);
+  H5::DataType type(const h5::vector<T>&) {
+    const auto subtype = type(T());
     return H5::VarLenType(&subtype);
   }
 
   template <typename M, typename T>
   void insert(H5::CompType& into, const std::string& name,
 	      size_t offset, M T::*) {
-    M dummy;
-    into.insertMember(name, offset, type(dummy));
+    into.insertMember(name, offset, type(M()));
   }
 
 }
